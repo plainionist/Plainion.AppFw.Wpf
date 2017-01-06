82b9109dfeb2f26ac448b4b42bd224ccab1db9da
ronin4net <pinguin751@gmx.net>
2017-01-06 14:43:22 +0100

    xml namespace definitions fixed

---
+++ src/Plainion.AppFw.Wpf/Properties/AssemblyDefs.cs
@@ -0,0 +1,4 @@
+﻿using System.Windows.Markup;
+[assembly: XmlnsPrefix( "http://github.com/ronin4net/plainion/appfw/wpf", "pnfw" )]
+[assembly: XmlnsDefinition( "http://github.com/ronin4net/plainion/appfw/wpf", "Plainion.AppFw.Wpf" )]
+[assembly: XmlnsDefinition( "http://github.com/ronin4net/plainion/appfw/wpf", "Plainion.AppFw.Wpf.Views" )]
